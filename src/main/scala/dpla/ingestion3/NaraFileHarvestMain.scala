b72ae472aeb0d3905c9b66f849b8352aa902411d
Michael Della Bitta <mdellabitta@gmail.com>
2017-04-26 11:29:02 -0400

    - minor style tweaks. - removed "MEMORY_AND_DISK" storage level.

--- src/main/scala/dpla/ingestion3/NaraFileHarvestMain.scala
+++ src/main/scala/dpla/ingestion3/NaraFileHarvestMain.scala
@@ -2,18 +2,14 @@ package dpla.ingestion3
 
 import java.io.File
 
+import com.databricks.spark.avro._
 import dpla.ingestion3.utils.Utils
 import org.apache.log4j.LogManager
 import org.apache.spark.SparkConf
-import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.storage.StorageLevel._
 
-import scala.util.Try
+
 import scala.xml.XML
-import com.databricks.spark.avro._
-import dpla.ingestion3.utils.MultiNodeSeq
-import org.apache.spark.storage.StorageLevel
 
 object NaraFileHarvestMain {
 
@@ -33,7 +29,7 @@ object NaraFileHarvestMain {
     }
     """
 
-  def main(args: Array[String]) = {
+  def main(args: Array[String]): Unit = {
 
     if(args.length != 3 ) {
       logger.error("Bad number of args: master input_file output_file")
@@ -56,10 +52,9 @@ object NaraFileHarvestMain {
     import spark.implicits._
     val sc = spark.sparkContext
     val strings = spark.read.avro(infile)
-    strings.persist(StorageLevel.MEMORY_AND_DISK)
     val stringPairs = strings.flatMap(row => handleXmlFile(row.getString(1)))
-    stringPairs.toDF("id", "document").write.format("com.databricks.spark.avro").option("avroSchema", schemaStr).avro(outfile)
     val recordCount = stringPairs.count()
+    stringPairs.toDF("id", "document").write.format("com.databricks.spark.avro").option("avroSchema", schemaStr).avro(outfile)
     sc.stop()
     val endTime = System.currentTimeMillis()
     Utils.printResults(endTime - startTime, recordCount)
@@ -68,7 +63,7 @@ object NaraFileHarvestMain {
 
   import dpla.ingestion3.utils.MultiNodeSeq._
 
-  def handleXmlFile(xmlString: String): Seq[Tuple2[String, String]] = for {
+  def handleXmlFile(xmlString: String): Seq[(String, String)] = for {
     xml <- XML.loadString(xmlString)
     item <- xml \\ Seq("item", "itemAv", "fileUnit")
     if (item \\ "digitalObjectArray").nonEmpty
